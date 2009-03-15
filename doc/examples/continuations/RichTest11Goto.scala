fbb06d07539895bc16d338b0bc1f0a1c0a96df6c
Rich Dougherty <rich@rd.gen.nz>
2009-03-15 15:59:50 +1300

    Added 'goto' example.

---
+++ doc/examples/continuations/RichTest11Goto.scala
@@ -0,0 +1,47 @@
+package examples.continuations
+
+import scala.continuations.ControlContext._
+
+case class Label(k: Label => Unit)
+
+object Label {
+
+  private case class GotoThunk(l: Label) extends Throwable
+
+  def label: Label @suspendable = shift { (k: Label => Unit) =>
+    val l = new Label(k)
+    def execute: Unit = {
+      try {
+        k(l)
+      } catch {
+        case GotoThunk(l0) if (l0 == l) => execute
+      }
+    }
+    execute
+  }
+
+  def goto(l: Label): Nothing = throw new GotoThunk(l)
+
+}
+
+object RichTest11Goto {
+
+  def main(args: Array[String]) = {
+    import Label._
+
+    reset {
+      var sum = 0
+      var i = 0
+      val beforeLoop = label
+      if (i < 10) {
+        println(i)
+        sum += i
+        i += 1
+        goto(beforeLoop)
+      }
+      println(sum)
+    }
+
+  }
+
+}
